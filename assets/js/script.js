63954cd23a3564e2a72e8e532a180af8c291f0f2
unknown <nickrizzo77277@gmail.com>
2021-04-23 16:40:46 -0600

    youtube test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-$('.dropdown-trigger').dropdown();
+//$('.dropdown-trigger').dropdown();
+var apiUrl =
+    'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyANhit3zRIVp92To57bvDn81fSUloR_QY4&type=video&q=monopoly'
+//--header 'Authorization: Bearer [YOUR_ACCESS_TOKEN]' \
+//--header 'Accept: application/json' \
+//--compressed
+fetch(apiUrl, {
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+    .then(response => {
+        console.log(response);
+        // handle the response
+    })
+
+    .catch(error => {
+        console.log(error);
+        // handle the error
+    });
+
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => console.log(data))
