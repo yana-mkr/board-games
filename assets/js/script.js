450ece70e5aa4e98dfa82c190aca89ac30c2e4de
unknown <nickrizzo77277@gmail.com>
2021-04-23 16:50:29 -0600

    added youtube function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,28 @@
 //$('.dropdown-trigger').dropdown();
-var apiUrl =
-    'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyANhit3zRIVp92To57bvDn81fSUloR_QY4&type=video&q=monopoly'
-//--header 'Authorization: Bearer [YOUR_ACCESS_TOKEN]' \
-//--header 'Accept: application/json' \
-//--compressed
-fetch(apiUrl, {
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
-    .then(response => {
-        console.log(response);
-        // handle the response
+var youtube = function () {
+
+    var apiUrl =
+        'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyANhit3zRIVp92To57bvDn81fSUloR_QY4&type=video&q=monopoly'
+    //--header 'Authorization: Bearer [YOUR_ACCESS_TOKEN]' \
+    //--header 'Accept: application/json' \
+    //--compressed
+    fetch(apiUrl, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
     })
+        .then(response => {
+            console.log(response);
+            // handle the response
+        })
 
-    .catch(error => {
-        console.log(error);
-        // handle the error
-    });
+        .catch(error => {
+            console.log(error);
+            // handle the error
+        });
+}
 
-fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => console.log(data))
+//fetch(apiUrl)
+//.then(response => response.json())
+//.then(data => console.log(data))
+youtube()
