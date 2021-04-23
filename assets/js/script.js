d0aad3bb9d5eae55a83b57b0fb8c9f298c796828
yana-mkr <yana.mkrtchyan@gmail.com>
2021-04-23 18:58:07 -0500

    cards displaying, not with all info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var cardDiv = document.getElementsByClassName("card")
+var cardDiv = document.querySelector(".games")
 
 $('.dropdown-trigger').dropdown();
 
@@ -10,36 +10,49 @@ var pullGames = function () {
             if (response.ok) {
                 console.log(response);
                 response.json().then(function (data) {
-                    console.log(data);
-                    console.log(data.games[0].name)
-                })
 
+                    console.log(data);
+                    //console.log(data.games[0].name)
+
+                    for (var i = 0; i < data.games.length; i++) {
+                        // how to limit to 10?
+                        var card = document.createElement("div")
+                        card.classList.add("card")
+                        cardDiv.appendChild(card)
+
+                        var title = document.createElement("h4")
+                        title.classList.add("card-content")
+                        title.textContent = data.games[i].name
+                        card.appendChild(title)
+
+                        // how to pull image as data and not URL?
+                        // var img = document.createElement("img")
+                        // img.textContent = data.games.thumb_url
+                        // card.append(img)
+
+                        var price = document.createElement("p")
+                        price.classList.add("card-content")
+                        price.textContent = "$" + data.games[i].price
+                        card.appendChild(price)
+
+                        // https://stackoverflow.com/questions/4772774/how-do-i-create-a-link-using-javascript
+                        var buyLink = document.createElement("a")
+                        var link1Text = document.createTextNode("Buy Here")
+                        buyLink.appendChild(link1Text)
+                        buyLink.href = data.games[i].url
+                        buyLink.classList.add("card-content")
+                        card.appendChild(buyLink)
+
+                        // var ytLink = document.createElement("a")
+                        // var link2Text = document.createTextNode ("Game Tutorial")
+                        // ytLink.appendChild(link2Text)
+                        // ytLink.href = pull youtube ytLink
+                        // card.append(ytLink)
+                    }
+                })
             }
 
         });
-    for (var i = 0; i < 10; i++) {
-        // set length to 10 games
-        var card = document.createElement("div")
-        card.textContent = data.games[i].name
-        card.classList.add("card")
-        cardDiv.append(card)
 
-        // how to pull image as data and not URL?
-        // var img = document.createElement("img")
-        // img.textContent = data.games.thumb_url
-        // cardDiv.append(img)
-
-        var price = document.createElement("p")
-        price.textContent = data.games[i].price
-        cardDiv.append(price)
-
-        var buyLink = document.createElement("a")
-        buyLink.textContent = data.games[i].url
-        cardDiv.append(buyLink)
-
-        // var ytLink = document.createElement("a")
-        // ytLink.textContent = pull youtube ytLink
-        // cardDiv.append(ytLink)
-    }
 }
 pullGames()
