1ba33eda5e669990f1c271d9a762807ba62b0565
Nick Rizzo <nickrizzo77277@gmail.com>
2021-04-24 11:24:27 -0600

    cleaned up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,15 @@ var youtube = function () {
 
     var apiUrl =
         'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyANhit3zRIVp92To57bvDn81fSUloR_QY4&type=video&q=howtoplay + clue'
-    //--header 'Authorization: Bearer [YOUR_ACCESS_TOKEN]' \
-    //--header 'Accept: application/json' \
-    //--compressed
+
     fetch(apiUrl, {
         headers: {
             'Content-Type': 'application/json'
         },
-        // params: {
-        //     part: 'snippet',
-        //     q: 'clue',
-        //     type: 'video',
-        //     key: 'AIzaSyANhit3zRIVp92To57bvDn81fSUloR_QY4'
-        // }
+
     })
         .then(response => {
             return response.json()
-            //console.log(response);
-            // handle the response
         }).then(data => {
             console.log(data);
             var videoId = data.items[0].id.videoId
@@ -31,12 +22,9 @@ var youtube = function () {
 
         .catch(error => {
             console.log(error);
-            // handle the error
         });
 }
 
-//fetch(apiUrl)
-//.then(response => response.json())
-//.then(data => console.log(data))
+
 youtube()
 
