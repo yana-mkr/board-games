f26d3fff4e1f3b7ef9253dfac3edb3e2b4eea26e
Davsav <davidbyu6@gmail.com>
2021-04-29 20:13:26 -0600

    added paramaters and adapted CSS to be mobile friendly on the cards and title

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,22 @@
 var cardDiv = document.querySelector(".games")
 var searchBtn = document.getElementById("search")
+var submitForm = document.getElementById("submit-form")
 
-var ageSelect = parseInt(document.querySelector("#age-select").value)
-var playerSelect = parseInt(document.querySelector("#player-select").value)
-var timeSelect = parseInt(document.querySelector("#time-select").value)
-
-
-// $('.dropdown-trigger').dropdown();
 $(document).ready(function(){
     $('select').formSelect();
   });
 
 var youtube = function (card, searchTerm) {
-    var apiYoutube =
-        'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyAB2XQ98P3h7KXGdn5XV2P09buyExiPDT4&type=video&q=howtoplay%20' + searchTerm
-    //console.log(apiYoutube)
+    var apiYoutube ='https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyAB2XQ98P3h7KXGdn5XV2P09buyExiPDT4&type=video&q=howtoplay%20' + searchTerm;
     return fetch(apiYoutube, {
         headers: {
             'Content-Type': 'application/json'
         },
     }).then(response => {
+        console.log(response)
         return response.json()
     }).then(data => {
+        console.log(data);
         var videoId = data.items[0].id.videoId
         var videoUrl = 'https://www.youtube.com/watch?v=' + videoId
         var ytLink = document.createElement("a")
@@ -29,23 +24,20 @@ var youtube = function (card, searchTerm) {
         ytLink.appendChild(link2Text)
         ytLink.href = videoUrl
         card.append(ytLink)
-    })
-        .catch(error => {
+    }).catch(error => {
             console.error(error);
         });
 }
 // create function for API call based on age, time, players.
 var pullGames = function (minAge, minPlayers, maxPlayTime) {
-    var apiURL = `https://api.boardgameatlas.com/api/search?order_by=popularity&min_players=${minPlayers}&max_playtime=${maxPlayTime}&min_age=${minAge}&client_id=JLBr5npPhV`
-
+    var apiURL = `https://api.boardgameatlas.com/api/search?order_by=popularity&min_players=${minPlayers}&max_playtime=${maxPlayTime}&min_age=${minAge}&client_id=JLBr5npPhV`;
     fetch(apiURL)
         .then(function (response) {
             if (response.ok) {
                 console.log(response);
-                response.json().then(function (data) {
-
+                response.json()
+                .then(function (data) {
                     console.log(data);
-                    // console.log(data).
                     console.log(data.games[0].name, "min age:" + data.games[0].min_age, "max playtime:" + data.games[0].max_playtime, "min players:" + data.games[0].min_players)
                     if (data.count === 0) {
                         var errorCard = document.createElement("div")
@@ -66,13 +58,12 @@ var pullGames = function (minAge, minPlayers, maxPlayTime) {
 
                         for (var i = 0; i < data.games.length; i++) {
                             if (i < 10) {
-                                // how to limit to 10?
                                 var card = document.createElement("div")
                                 card.classList.add("card")
                                 cardDiv.appendChild(card)
 
-                                var title = document.createElement("h3")
-                                title.classList.add("card-content")
+                                var title = document.createElement("div")
+                                title.classList.add("title")
                                 title.textContent = data.games[i].name
                                 card.appendChild(title)
 
@@ -93,50 +84,44 @@ var pullGames = function (minAge, minPlayers, maxPlayTime) {
                                 buyLink.href = data.games[i].url
                                 buyLink.classList.add("card-content")
                                 card.appendChild(buyLink)
-
                                 youtube(card, data.games[i].name)
                             }
                         }
                     }
                 })
             }
-
         })
         .catch(error => {
             console.error(error);
         });
 }
 
+// function gameSearch (ageSelect, playerSelect, timeSelect){
+//     var baseUrl = `https://api.boardgameatlas.com/api/search?order_by=popularity&min_players=${playerSelect}&max_playtime=${timeSelect}&min_age=${ageSelect}&client_id=JLBr5npPhV`;
+//     fetch(baseUrl)
+//     .then(function (response) {
+//         if (response.ok) {
+//             console.log(response);
+//             response.json()
+//             .then(function (data) {
+//                 console.log(data)
+//             })
+// }})}
 
-
-function gameSearch (ageSelect, playerSelect, timeSelect){
-    // e.preventDefault()
-    var baseUrl = `https://api.boardgameatlas.com/api/search?order_by=popularity&min_players=${playerSelect}&max_playtime=${timeSelect}&min_age=${ageSelect}&client_id=JLBr5npPhV`
-
-    fetch(baseUrl)
-    .then(function (response) {
-        if (response.ok) {
-            console.log(response);
-            response.json().then(function (data) {
-                console.log(data)
-                console.log(ageSelect,playerSelect,timeSelect)
-
-            })
-}})}
-console.log(ageSelect,playerSelect,timeSelect)
-
-gameSearch (ageSelect,playerSelect,timeSelect)
-
-// randomBtn.addEventListener("click", function () {
-//     var search = "name="
-//     pullGames(search)
-// })
-// gameSearch ()
-// console.log (minAge)
-// console.log (minPlayers)
-// console.log (maxPlayTime)
-//pullGames()
-
-searchBtn.addEventListener("click", function () {
-    pullGames(10, 4, 30)
-})
+submitForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let cards = document.querySelectorAll('.card')
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].replaceWith('');
+    };
+    console.log(event)
+    let age = event.target[1].value;
+    let player = event.target[3].value;
+    let time = event.target[5].value;
+    console.log(age, player, time)
+    // gameSearch(age,player, time);
+    pullGames(age, player, time);
+    
+    var change = document.querySelector(".gameDisp");
+    change.classList.add("gameDispTwo");
+});
