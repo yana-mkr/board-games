77f9a5974158f1a6b34e836f8f5f35cdc2d17924
yana-mkr <yana.mkrtchyan@gmail.com>
2021-05-01 21:09:43 -0500

    local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,13 @@ var resetBtn = document.getElementById("reset")
 var searchBtn = document.getElementById("search")
 var submitForm = document.getElementById("submit-form")
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('select').formSelect();
-  });
+});
 
 var youtube = function (card, searchTerm) {
 
-    var apiYoutube ='https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyAB2XQ98P3h7KXGdn5XV2P09buyExiPDT4&type=video&q=howtoplay%20' + searchTerm;
+    var apiYoutube = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyAB2XQ98P3h7KXGdn5XV2P09buyExiPDT4&type=video&q=howtoplay%20' + searchTerm;
 
     return fetch(apiYoutube, {
         headers: {
@@ -28,8 +28,8 @@ var youtube = function (card, searchTerm) {
         ytLink.href = videoUrl
         card.append(ytLink)
     }).catch(error => {
-            console.error(error);
-        });
+        console.error(error);
+    });
 }
 
 
@@ -41,59 +41,59 @@ var pullGames = function (minAge, minPlayers, maxPlayTime) {
             if (response.ok) {
                 console.log(response);
                 response.json()
-                .then(function (data) {
-                    console.log(data);
-                    console.log(data.games[0].name, "min age:" + data.games[0].min_age, "max playtime:" + data.games[0].max_playtime, "min players:" + data.games[0].min_players)
-                    if (data.count === 0) {
-                        var errorCard = document.createElement("div")
-                        errorCard.classList.add("card")
+                    .then(function (data) {
+                        console.log(data);
+                        console.log(data.games[0].name, "min age:" + data.games[0].min_age, "max playtime:" + data.games[0].max_playtime, "min players:" + data.games[0].min_players)
+                        if (data.count === 0) {
+                            var errorCard = document.createElement("div")
+                            errorCard.classList.add("card")
 
-                        var img = document.createElement("img")
-                        img.setAttribute("src", "https://images.unsplash.com/photo-1606823616058-541d59dadcb2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80")
-                        img.classList.add("img")
-                        errorCard.appendChild(img)
+                            var img = document.createElement("img")
+                            img.setAttribute("src", "https://images.unsplash.com/photo-1606823616058-541d59dadcb2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80")
+                            img.classList.add("img")
+                            errorCard.appendChild(img)
 
-                        var errorMsg = document.createElement("h3")
-                        errorMsg.textContent = "There has been an error, please try your search again!"
-                        errorCard.appendChild(errorMsg)
+                            var errorMsg = document.createElement("h3")
+                            errorMsg.textContent = "There has been an error, please try your search again!"
+                            errorCard.appendChild(errorMsg)
 
-                        cardDiv.appendChild(errorCard)
+                            cardDiv.appendChild(errorCard)
 
-                    } else {
+                        } else {
 
-                        for (var i = 0; i < data.games.length; i++) {
-                            if (i < 10) {
-                                var card = document.createElement("div")
-                                card.classList.add("card")
-                                cardDiv.appendChild(card)
+                            for (var i = 0; i < data.games.length; i++) {
+                                if (i < 10) {
+                                    var card = document.createElement("div")
+                                    card.classList.add("card")
+                                    cardDiv.appendChild(card)
 
-                                var title = document.createElement("div")
-                                title.classList.add("title")
-                                title.textContent = data.games[i].name
-                                card.appendChild(title)
+                                    var title = document.createElement("div")
+                                    title.classList.add("title")
+                                    title.textContent = data.games[i].name
+                                    card.appendChild(title)
 
-                                var img = document.createElement("img")
-                                img.setAttribute("src", data.games[i].thumb_url)
-                                img.classList.add("img")
-                                card.append(img)
+                                    var img = document.createElement("img")
+                                    img.setAttribute("src", data.games[i].thumb_url)
+                                    img.classList.add("img")
+                                    card.append(img)
 
-                                var price = document.createElement("p")
-                                price.classList.add("card-content")
-                                price.textContent = "$" + data.games[i].price
-                                card.appendChild(price)
+                                    var price = document.createElement("p")
+                                    price.classList.add("card-content")
+                                    price.textContent = "$" + data.games[i].price
+                                    card.appendChild(price)
 
-                                // https://stackoverflow.com/questions/4772774/how-do-i-create-a-link-using-javascript
-                                var buyLink = document.createElement("a")
-                                var link1Text = document.createTextNode("Buy Here")
-                                buyLink.appendChild(link1Text)
-                                buyLink.href = data.games[i].url
-                                buyLink.classList.add("card-content")
-                                card.appendChild(buyLink)
-                                youtube(card, data.games[i].name)
+                                    // https://stackoverflow.com/questions/4772774/how-do-i-create-a-link-using-javascript
+                                    var buyLink = document.createElement("a")
+                                    var link1Text = document.createTextNode("Buy Here")
+                                    buyLink.appendChild(link1Text)
+                                    buyLink.href = data.games[i].url
+                                    buyLink.classList.add("card-content")
+                                    card.appendChild(buyLink)
+                                    youtube(card, data.games[i].name)
+                                }
                             }
                         }
-                    }
-                })
+                    })
             }
         })
         .catch(error => {
@@ -106,29 +106,21 @@ resetBtn.addEventListener('click', function () {
     cardDiv.innerHTML = "";
 })
 
-//pullGames()
-
-// function gameSearch (ageSelect, playerSelect, timeSelect){
-//     var baseUrl = `https://api.boardgameatlas.com/api/search?order_by=popularity&min_players=${playerSelect}&max_playtime=${timeSelect}&min_age=${ageSelect}&client_id=JLBr5npPhV`;
-//     fetch(baseUrl)
-//     .then(function (response) {
-//         if (response.ok) {
-//             console.log(response);
-//             response.json()
-//             .then(function (data) {
-//                 console.log(data)
-//             })
-// }})}
-function searchHistory(age, player, time,){
+function searchHistory(age, player, time,) {
     var savedParameters = JSON.parse(window.localStorage.getItem("savedParameters")) || []
+    var savedSearches = document.querySelector("#saved-searches")
     var savedUserChoice = {
         age: age,
         player: player,
         time: time,
     }
-   savedParameters.push(savedUserChoice)
-   localStorage.setItem("savedParameters",JSON.stringify(savedParameters))
-   }
+
+    savedParameters.push(savedUserChoice)
+    localStorage.setItem("savedParameters", JSON.stringify(savedParameters))
+    savedSearches.appendChild("savedUserChoice")
+}
+
+
 
 submitForm.addEventListener("submit", function (event) {
     event.preventDefault();
